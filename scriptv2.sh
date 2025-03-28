35a2e58b5e03ba73300bd3d15178999bfdfbdc6f
Ubuntu <ubuntu@ip-172-31-14-197.ap-south-1.compute.internal>
2025-03-28 09:03:43 +0000

    added token and scriptv2

---
+++ scriptv2.sh
@@ -0,0 +1,10 @@
+echo "Hello Rockstar....."
+
+echo "Please wait , the Build is processing"
+
+echo "...................................................."
+
+echo "............Build Details...................."
+
+echo whoami
+echo hostname
